8416d2870842c753a454b126e3ee2fd0590c7b32
dheenesh143king <101351316+dheenesh143king@users.noreply.github.com>
2022-12-20 19:45:30 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function startClassification(){
+    navigator.mediaDevices.getUserMedia({audio: true});
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/oADGVspxp/model.json',modelReady);
+}
+function modelReady(){
+    classifier.classify(gotResults);
+}
